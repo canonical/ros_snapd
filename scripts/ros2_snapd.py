884606f4c5cb8432ee65aa372d637b6c52f1aa94
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2025-11-27 10:27:49 +0100

    fix(snap): use the new ros-snapd-support interface

--- scripts/ros2_snapd.py
+++ scripts/ros2_snapd.py
@@ -25,6 +25,10 @@ from rclpy.node import Node
 import time
 
 
+# Necessary to use the snap socket for interfaces
+snap_http.http.SNAPD_SOCKET = "/run/snapd-snap.socket"
+
+
 _EXCLUSION_LIST = [
     "lxd.activate",
     "lxd.daemon",
